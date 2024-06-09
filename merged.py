638d146c1564e7afe8054e56e2df795ff14ea394
William Borelli Ebert <156264931+williamborelli77@users.noreply.github.com>
2024-06-08 19:20:35 -0700

    Update merged.py

--- merged.py
+++ merged.py
@@ -245,6 +245,60 @@ print(result4.summary)
 Overall, the OLS models indicate that we cannot definitively determine whether the effect of Uber on public transit was complementary or supplementary. The results are not robust across regressions, and therefore, we cannot establish causality. This inconsistency suggests that further analysis with more rigorous methods is needed to understand the true impact of Uber on public transit ridership. Below, we try to address these problems using LASSO and double LASSO regression techniques.
 """
 
+#### Evaluate Residuals for OLS Models
+### Plot 1
+#Create Residuals
+ols3_res= result3.resids
+ols4_res = result4.resids
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(14, 7), constrained_layout=True)
+
+# First plot
+axes[0].scatter(ols3_res, np.log(data['UPTTotal']), label='Residuals', 
+                color='b', alpha=0.6, edgecolor='w', s=80)
+axes[0].set_xlabel('Residuals', fontsize=12)
+axes[0].set_ylabel('Log of UPTTotal', fontsize=12)
+axes[0].set_title('Residuals vs. UPTTotal for OLS with DP Interaction', fontsize=14)
+axes[0].legend()
+axes[0].grid(True, which='both', linestyle='--', linewidth=0.7)
+
+# Second plot
+axes[1].scatter(ols4_res, np.log(data['UPTTotal']), label='Residuals', 
+                color='r', alpha=0.6, edgecolor='w', s=80)
+axes[1].set_xlabel('Residuals', fontsize=12)
+axes[1].set_ylabel('Log of UPTTotal', fontsize=12)
+axes[1].set_title('Residuals vs. UPTTotal for OLS with DF Interaction', fontsize=14)
+axes[1].legend()
+axes[1].grid(True, which='both', linestyle='--', linewidth=0.7)
+
+plt.show()
+
+###Plot 2
+# Create Figure
+obs_count = list(range(1, len(ols3_res) + 1))
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(14, 7), constrained_layout=True)
+
+# First plot
+axes[0].plot(obs_count, ols3_res, label='Residual', color='b', linewidth=2, 
+             marker='o', markersize=5, markerfacecolor='white')
+axes[0].set_xlabel('Observation', fontsize=12)
+axes[0].set_ylabel('Residuals', fontsize=12)
+axes[0].set_title('Residuals with DP Interaction', fontsize=14)
+axes[0].legend()
+axes[0].grid(True, which='both', linestyle='--', linewidth=0.7)
+
+# Second plot
+axes[1].plot(obs_count, ols4_res, label='Residual', color='r', linewidth=2, 
+             marker='o', markersize=5, markerfacecolor='white')
+axes[1].set_xlabel('Observation', fontsize=12)
+axes[1].set_ylabel('Residual', fontsize=12)
+axes[1].set_title('Residuals OLS with DF Interaction', fontsize=14)
+axes[1].legend()
+axes[1].grid(True, which='both', linestyle='--', linewidth=0.7)
+
+plt.show()
+
 # %% [markdown]
 # <div style="border: 1px solid black; border-radius: 5px; overflow: hidden;">
 #     <div style="background-color: black; color: white; padding: 5px; text-align: left;">
